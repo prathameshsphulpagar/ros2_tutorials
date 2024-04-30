12f41c84c1d6a0fb95699bc645b320fb048f1c63
Prathaemsh Phulpagar <prathameshphulpagar8p@gmail.com>
2024-04-30 15:33:19 +0530

    creating the ros2 server file incomplete file.

---
+++ Level3_ws/src/actions_cpp/src/count_until_clients.cpp
@@ -0,0 +1,31 @@
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "my_robot_interfaces/action/count_until.hpp"
+
+using CountUntil = my_robot_interfaces::action::CountUntil;
+class CountUntilClientsNode : public rclcpp::Node // MODIFY NAME
+{
+public:
+    CountUntilClientsNode() : Node("CountUntilClientsNode") // MODIFY NAME
+    {
+        count_until_client_ = rclcpp_action::create_client<CountUntil>(this,"count_until");
+    }
+
+    void send_goal(int target_number,double period)
+    {
+         // send the value to the client node.
+
+    }
+private:
+    rclcpp_action::Client<CountUntil>::SharedPtr count_until_client_;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<CountUntilClientsNode>(); // MODIFY NAME
+    node->send_goal(6, 0.8);
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
