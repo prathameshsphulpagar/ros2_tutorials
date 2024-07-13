b3f45c30927a928355ff872bd4987325e2d52f9b
Prathaemsh Phulpagar <prathamesh@collabint.com>
2024-07-14 01:17:43 +0530

    basic sright line publish to /cmd

--- path_planning_ws/src/maze_bot/setup.py
+++ path_planning_ws/src/maze_bot/setup.py
@@ -30,6 +30,7 @@ setup(
             'go_to_goal = maze_bot.go_to_goal:main',
             'video_recorder = maze_bot.video_saver:main',
             'maze_solver = maze_bot.maze_solver:main',
+            'driving_mazebot = maze_bot.driving_node:main',
         ],
     },
 )
